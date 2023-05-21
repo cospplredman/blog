efba4fb694522d0d31378dc8480ced49bf8f5eb2
cospplredman <cospplredman@gmail.com>
2023-05-21 17:24:57 -0500

    initial blog setup stuff

---
+++ posts.js
@@ -0,0 +1,19 @@
+
+/*
+	stores metadata for posts
+
+	format:
+		{ title: title, id: id }
+
+	where title is the title of the post and id is the post number
+*/
+
+
+let posts = [
+	{
+		title: "inital post",
+		id: 0
+	}
+];
+
+
